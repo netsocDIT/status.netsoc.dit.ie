c77df08ef8ddbb29a9b3ebf73f16cf5e39ddb13f
Graham Hayes <graham@hayes.ie>
2013-02-17 22:57:02 +0000

    Basic site in place

---
+++ issues.json.js
@@ -0,0 +1,28 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: Graham
+ * Date: 17/02/13
+ * Time: 22:00
+ * To change this template use File | Settings | File Templates.
+ */
+issues = [
+    {
+        type:"Outage",
+        date:"17/02/2013",
+        title:"Chef Down",
+        level:3,
+        desc:"we are having issue on our login server chef, we will update with more info soon.",
+        fixed:true
+    }
+]
+
+downtime = [
+    {
+        type:"Outage",
+        date:"17/02/2013",
+        title:"Network Upgrades",
+        duration:"2 hrs",
+        desc:"we are having issue on our login server chef, we will update with more info soon.",
+        fixed:false
+    }
+]
---
+++ page.js
@@ -0,0 +1,67 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: Graham
+ * Date: 17/02/13
+ * Time: 22:10
+ * To change this template use File | Settings | File Templates.
+ */
+for (issue in issues) {
+    var block = document.createElement('div');
+    var icon = document.createElement('i');
+    var label = document.createElement('span');
+    var heading = document.createElement('h3');
+    var desc = document.createElement('p');
+    $(block).addClass('well');
+    $(icon).addClass('icon-2x pull-right');
+    $(label).addClass('label');
+
+    if (issues[issue].fixed){
+        $(label).text("Fixed: " + issues[issue].type);
+        $(label).addClass('label-success');
+        if (issues[issue].level == 3){
+            $(icon).addClass('icon-fire');
+        } else if (issues[issue].level == 2){
+            $(icon).addClass('icon-warning-sign');
+        } else if (issues[issue].level == 1){
+            $(icon).addClass('icon-info-sign');
+        }
+    } else {
+        $(label).text(issues[issue].type);
+        if (issues[issue].level == 3){
+            $(label).addClass('label-important');
+            $(icon).addClass('icon-fire');
+        } else if (issues[issue].level == 2){
+            $(label).addClass('label-warning');
+            $(icon).addClass('icon-warning-sign');
+        } else if (issues[issue].level == 1){
+            $(label).addClass('label-info');
+            $(icon).addClass('icon-info-sign');
+        }
+    }
+    $(heading).html(issues[issue].title + "<small> : "+ issues[issue].date +"</small>");
+    $(desc).text(issues[issue].desc )
+    $(block).append(icon);
+    $(block).append(label);
+    $(block).append(heading);
+    $(block).append(desc);
+    $(block).appendTo('#outages-container');
+}
+for (issue in downtime) {
+    var block = document.createElement('div');
+    var icon = document.createElement('i');
+    var label = document.createElement('span');
+    var heading = document.createElement('h3');
+    var desc = document.createElement('p');
+    $(block).addClass('well');
+    $(icon).addClass('icon-2x pull-right');
+    $(label).addClass('label label-info');
+    $(label).text(downtime[issue].type);
+
+    $(heading).html(downtime[issue].title + "<small> : "+ downtime[issue].date +" - Duration:"+ downtime[issue].duration +"</small>");
+    $(desc).text(downtime[issue].desc )
+    $(block).append(icon);
+    $(block).append(label);
+    $(block).append(heading);
+    $(block).append(desc);
+    $(block).appendTo('#downtime-container');
+}
